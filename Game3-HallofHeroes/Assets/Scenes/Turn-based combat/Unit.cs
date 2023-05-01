0996cbae18622015a01b67c0813499d6d049a171
Chris French <cf208ebf@gmail.com>
2023-04-30 21:53:26 -0500

    Turn based alpha
    Very poor first attempt at a combat system.

---
+++ Game3-HallofHeroes/Assets/Scenes/Turn-based combat/Unit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+	public string unitName;
+
+	public int damage;
+	public int defense;
+
+	public int maxHP;
+	public int currentHP;
+
+	public bool TakeDamage(int dmg)
+	{
+		if(defense < dmg)
+			currentHP -= (dmg - defense);
+		else
+			currentHP -= 1;
+
+		if (currentHP <= 0)
+			return true;
+		else
+			return false;
+	}
+
+	public void Heal(int amount)
+	{
+		currentHP += amount;
+		if (currentHP > maxHP)
+			currentHP = maxHP;
+	}
+
+}
