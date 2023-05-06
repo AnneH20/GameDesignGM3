0b23294c11e6e1b35e2441cc3b136e4689685aca
AnneH20 <alh1310@msstate.edu>
2023-05-05 23:52:58 -0500

    Player Movement Animation for Level 1
    Didn't want to mess with the other levels bc I didn't know that the maps were for

--- Game3-HallofHeroes/Assets/Scripts/PlayerController.cs
+++ Game3-HallofHeroes/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+
 public class PlayerState
 {
     public int health;
@@ -22,6 +23,8 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     private static PlayerController instance;
     public static PlayerController Instance { get { return instance; } }
+    float moveX;
+    float moveY;
 
     void Start()
     {
@@ -29,12 +32,12 @@ public class PlayerController : MonoBehaviour
         maxHealth = playerState.maxHealth;
         rb.transform.position = new Vector2(0, 0);
         rb.gravityScale = 0f;
-        
+
     }
 
     private void Awake()
     {
-        
+
         if (instance == null)
         {
             instance = this;
@@ -46,33 +49,72 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
             Destroy(grid);
         }
-       
+
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
-        
+
+        // check for left and right arrow keys
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("horizontalWalk", true);
+        }
+        else
+        {
+            animator.SetBool("horizontalWalk", false);
+        }
     }
-    private void FixedUpdate()   
+
+    private void FixedUpdate()
     {
         Move();
     }
-        
-    
-    void ProcessInputs() 
+
+
+    void ProcessInputs()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        moveX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveY = Input.GetAxisRaw("Vertical") * moveSpeed;
 
         moveDirection = new Vector2(moveX, moveY);
 
+        if (moveX < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            animator.SetBool("verticalDown", false);
+            animator.SetBool("verticalUp", false);
+        }
+        else if (moveX > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            animator.SetBool("verticalDown", false);
+            animator.SetBool("verticalUp", false);
+        }
+        if (moveY < 0)
+        {
+            animator.SetBool("verticalUp", false);
+            animator.SetBool("verticalDown", true);
+        }
+        else if (moveY > 0)
+        {
+            animator.SetBool("verticalDown", false);
+            animator.SetBool("verticalUp", true);
+        }
+        else
+        {
+            animator.SetBool("verticalDown", false);
+            animator.SetBool("verticalUp", false);
+        }
+        animator.SetFloat("horizontalSpeed", Mathf.Abs(moveX));
+        animator.SetFloat("verticalSpeed", Mathf.Abs(moveY));
     }
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-    
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
